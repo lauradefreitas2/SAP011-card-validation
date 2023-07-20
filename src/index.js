8e8502162e8972eca2e40023e531d5b9c816f8ec
lauradefreitas2 <lauradovalegameplays@gmail.com>
2023-07-20 07:10:02 -0300

    adicionando os scripts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
+// Importa a função validateCreditCardNumber do arquivo './validator.js'.
 import validator from './validator.js';
 
-console.log(validator);
+// Seleciona o elemento <form> do documento HTML.
+const form = document.querySelector('form');
+// Seleciona o elemento de entrada de número do cartão de crédito pelo ID 'credit-card'.
+const cardNumberInput = document.getElementById('credit-card');
+// Seleciona o elemento de contêiner de resultado pelo ID 'result'.
+const resultContainer = document.getElementById('result');
+
+// Adiciona um ouvinte de evento para o evento 'submit' no formulário.
+form.addEventListener('submit', (event) => {
+  // Previne o comportamento padrão do formulário (envio da página).
+  event.preventDefault();
+
+  // Obtém o valor do número do cartão de crédito do campo de entrada.
+  const cardNumber = cardNumberInput.value;
+
+  // Verifica se o número do cartão está em branco ou contém apenas espaços.
+  if (cardNumber.trim() === '') {
+    // Exibe uma mensagem de erro no contêiner de resultado.
+    resultContainer.textContent = 'Por favor, insira um número de cartão de crédito válido.';
+    // Adiciona a classe 'error' ao contêiner para estilização específica de erro.
+    resultContainer.classList.add('error');
+    // Encerra a execução da função.
+    return;
+  }
+
+  // Chama a função validateCreditCardNumber para validar o número do cartão.
+  // A função retorna um objeto com as propriedades "isValid" e "maskedCardNumber".
+  const isValid = validator.isValid(cardNumber);
+
+  // Verifica se o número do cartão é válido.
+  if (isValid) {
+    // Exibe uma mensagem de sucesso com o número do cartão mascarado no contêiner de resultado.
+    resultContainer.textContent = `O número do cartão de crédito é válido: ${validator.maskify(cardNumber)}`;
+    // Remove a classe 'error' do contêiner para limpar a estilização de erro, se estiver presente.
+    resultContainer.classList.remove('error');
+  } else {
+    // Exibe uma mensagem de erro no contêiner de resultado.
+    resultContainer.textContent = 'O número do cartão de crédito é inválido.';
+    // Adiciona a classe 'error' ao contêiner para estilização específica de erro.
+    resultContainer.classList.add('error');
+  }
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,50 @@
 const validator = {
-  // ...
-};
+  maskify:function (cardNumber) {
+    // Substitui todos os dígitos, exceto os últimos 4, por '#'.
+    const maskedDigits = cardNumber.slice(0, -4).replace(/\d/g, '#');
+    // Seleciona os últimos 4 dígitos do número do cartão de crédito.
+    const visibleDigits = cardNumber.slice(-4);
+    // Retorna a concatenação dos dígitos mascarados e dos últimos 4 dígitos visíveis.
+    return maskedDigits + visibleDigits;
+  },
+  isValid: function isValid(cardNumber) {
+    // Remove todos os caracteres não numéricos do número do cartão.
+    const cleanedCardNumber = cardNumber.replace(/\D/g, '');
+    // Inicializa a variável "sum" com 0 para calcular a soma dos dígitos.
+    let sum = 0;
+    // Indica se o dígito atual é o segundo dígito a ser processado.
+    let isSecondDigit = false;
+  
+    // Itera através dos dígitos do número do cartão de crédito, começando do último dígito.
+    for (let i = cleanedCardNumber.length - 1; i >= 0; i--) {
+      // Converte o dígito atual em um número inteiro.
+      let digit = parseInt(cleanedCardNumber.charAt(i));
+  
+      // Verifica se o dígito atual deve ser multiplicado por 2.
+      if (isSecondDigit) {
+        digit *= 2;
+  
+        // Se o resultado da multiplicação for maior ou igual a 10, soma os dígitos individualmente.
+        if (digit >= 10) {
+          digit = digit.toString();
+          digit = parseInt(digit.charAt(0)) + parseInt(digit.charAt(1));
+        }
+      }
+  
+      // Adiciona o dígito (ou a soma dos dígitos, no caso de multiplicação por 2) à variável "sum".
+      sum += digit;
+      // Alterna a flag "isSecondDigit" para indicar o próximo dígito.
+      isSecondDigit = !isSecondDigit;
+    }
+  
+    
+  
+    // Retorna um objeto com a validade do número do cartão e o número mascarado.
+    return  sum % 10 === 0;
+  }
+
+}
 
 export default validator;
+  
+  
